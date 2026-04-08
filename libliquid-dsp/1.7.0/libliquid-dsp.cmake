df41d423d2ccecf332ad80de988e5cc7d85df012
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2026-04-09 00:17:03 +0200

    libliquid_dsp: added the init cmake file for liquid dsp lib.

---
+++ libliquid-dsp/1.7.0/libliquid-dsp.cmake
@@ -0,0 +1,25 @@
+function(liquid-dsp_Populate remote_url local_path OS ARCH BUILD_TYPE)
+
+    set(src_dir ${local_path}/src)
+    set(build_dir ${local_path}/build)
+    set(install_dir ${local_path}/install)
+
+    # v1.7.0 branch
+    if(NOT EXISTS ${src_dir})
+        execute_process(COMMAND git clone https://github.com/jgaeddert/liquid-dsp/tree/v1.7.0 ${src_dir})
+    endif()
+
+    execute_process(COMMAND ${CMAKE_COMMAND}
+            -S ${src_dir}
+            -B ${build_dir}
+            -DCMAKE_INSTALL_PREFIX=${install_dir}
+            -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+    )
+
+    execute_process(COMMAND ${CMAKE_COMMAND} --build ${build_dir} --target install)
+
+    set_property(GLOBAL PROPERTY liquid-dsp_INCLUDE_DIRS ${install_dir}/include)
+    set_property(GLOBAL PROPERTY liquid-dsp_LIBRARIES ${install_dir}/lib/libliquid.a)
+    set_property(GLOBAL PROPERTY liquid-dsp_INSTALL_LIBRARIES ${install_dir}/lib/libliquid.a)
+
+endfunction()
